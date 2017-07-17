d94c410512ec24cd3a88486a11fdd06b79988a4a
Yilin Liu <YLIU07@beckman.com>
2017-07-17 16:25:24 -0400

    add index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/ui" => "ui#index"
+  get "/ui#" => "ui#index"
+  root "ui#index"
 end
