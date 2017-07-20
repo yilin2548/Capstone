c77e28ee164bca1607590e5e052507b4e07df2b7
Yilin Liu <YLIU07@beckman.com>
2017-07-20 14:45:28 -0400

    Add angular module spa

---
+++ app/assets/javascripts/spa/app.router.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('spa')
+        .config(RouterFunction);
+
+    RouterFunction.$inject = ['$stateProvider',
+                              '$urlRouterProvider',
+                              'spa.APP_CONFIG'];
+
+    /* @ngInject */
+    function RouterFunction($stateProvider, $urlRouterProvider, APP_CONFIG) {
+        $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: APP_CONFIG.main_page_html,
+
+        })
+
+
+        $urlRouterProvider.otherwise('/');
+    }
+})();
